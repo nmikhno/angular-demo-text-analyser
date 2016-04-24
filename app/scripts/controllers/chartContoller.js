256c8995dcac040ce251fb362ed9d7be633d994f
Nikage <nmikhno@evergreens.com.ua>
2016-04-24 22:46:57 +0300

    Added comments

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -8,17 +8,22 @@
  */
 angular.module('sbAdminApp')
   .controller('ChartCtrl', ['$scope', 'textAnalyserService', function ($scope, textAnalyserService) {
+	  // by default input is empty
 	  $scope.textToAnalyse = '';
 
 	  $scope.analyseText = function(){
 
 		  $scope.wordStats = textAnalyserService.getWordsOccurence($scope.textToAnalyse, 2);
 
+		  // Display message in case the input
+		  // is too short to show chart
 		  if (_.size($scope.wordStats) == 0) {
 			  $scope.bar = {
 				  labels: ['The text is to short'],
 				  data: [['foo']]
 			  };
+
+
 		  }else if(_.size($scope.wordStats) > 0) {
 			  $scope.bar = {
 				  labels: Object.keys($scope.wordStats),
@@ -28,10 +33,8 @@ angular.module('sbAdminApp')
 
 	  };
 
-
-
-	  var isValidInputText = function(){
-		  return $scope.textToAnalyse.$valid;
-	  }
-
+	// Lodash dependency injection.
+	// It is quite possible not use this library here
+	// and it is a quick way to iterate over an object.
+	// Core version takes extra 14 kb but makes much more profit
   }]).constant('_', window._);
