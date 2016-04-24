3fefdfb2984a7e0cc2abad7bf67d265d31b9e15b
Nikage <nmikhno@evergreens.com.ua>
2016-04-24 18:46:07 +0300

    Deleted demo data, added controller's functions to process text data with textAnalyserService

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -7,63 +7,31 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('ChartCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-    $scope.line = {
-	    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	    series: ['Series A', 'Series B'],
-	    data: [
-	      [65, 59, 80, 81, 56, 55, 40],
-	      [28, 48, 40, 19, 86, 27, 90]
-	    ],
-	    onClick: function (points, evt) {
-	      console.log(points, evt);
-	    }
-    };
+  .controller('ChartCtrl', ['$scope', 'textAnalyserService', function ($scope, textAnalyserService) {
+	  $scope.textToAnalyse = '';
 
-    $scope.bar = {
-	    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-		series: ['Series A', 'Series B'],
+	  $scope.analyseText = function(){
 
-		data: [
-		   [65, 59, 80, 81, 56, 55, 40],
-		   [28, 48, 40, 19, 86, 27, 90]
-		]
-    	
-    };
+		  $scope.wordStats = textAnalyserService.getWordsOccurence($scope.textToAnalyse, 2);
 
-    $scope.donut = {
-    	labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
-    	data: [300, 500, 100]
-    };
+		  if (_.size($scope.wordStats) == 0) {
+			  $scope.bar = {
+				  labels: ['The text is to short'],
+				  data: [['foo']]
+			  };
+		  }else if(_.size($scope.wordStats) > 0) {
+			  $scope.bar = {
+				  labels: Object.keys($scope.wordStats),
+				  data: [_.values($scope.wordStats)]
+			  };
+		  }
 
-    $scope.radar = {
-    	labels:["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+	  };
 
-    	data:[
-    	    [65, 59, 90, 81, 56, 55, 40],
-    	    [28, 48, 40, 19, 96, 27, 100]
-    	]
-    };
 
-    $scope.pie = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
-    	data : [300, 500, 100]
-    };
 
-    $scope.polar = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
-    	data : [300, 500, 100, 40, 120]
-    };
+	  var isValidInputText = function(){
+		  return $scope.textToAnalyse.$valid;
+	  }
 
-    $scope.dynamic = {
-    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
-    	data : [300, 500, 100, 40, 120],
-    	type : 'PolarArea',
-
-    	toggle : function () 
-    	{
-    		this.type = this.type === 'PolarArea' ?
-    	    'Pie' : 'PolarArea';
-		}
-    };
-}]);
+  }]).constant('_', window._);
